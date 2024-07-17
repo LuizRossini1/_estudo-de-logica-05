1a65ee41b6fa0ac387e3423ef40f23d0be270b0a
LuizRossini <luiz.guirossini123@gmail.com>
2024-07-17 20:42:38 -0300

    instantiate objects, add to list and loop to traverse the list

--- src/Desafio01/Principal.java
+++ src/Desafio01/Principal.java
@@ -3,5 +3,20 @@ package Desafio01;
 import java.util.ArrayList;
 
 public class Principal {
+    public static void main(String[] args) {
+        ArrayList<Principal> lista = new ArrayList<>();
 
+        var objeto01 = new Principal();
+        var objeto02 = new Principal();
+        var objeto03 = new Principal();
+
+        lista.add(objeto01);
+        lista.add(objeto02);
+        lista.add(objeto03);
+
+        for (Principal index: lista) {
+            System.out.println(index);
+        }
+
+    }
 }
