34758431ac3b31304670333cc6294de30195c829
LuizRossini <luiz.guirossini123@gmail.com>
2024-07-17 20:36:24 -0300

    creating package and class, exercise 1

---
+++ src/Desafio01/Principal.java
@@ -0,0 +1,7 @@
+/* Crie um ArrayList de strings e utilize um loop foreach para percorrer e imprimir cada elemento da lista. */
+package Desafio01;
+import java.util.ArrayList;
+
+public class Principal {
+
+}
