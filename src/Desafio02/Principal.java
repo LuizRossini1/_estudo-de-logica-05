b3f6985580fbc9dc6c0f162972cf2c4c3b7fa08e
LuizRossini <luiz.guirossini123@gmail.com>
2024-07-17 21:29:00 -0300

    creating verification to make casting

--- src/Desafio02/Principal.java
+++ src/Desafio02/Principal.java
@@ -1,8 +1,16 @@
+/* 3 - Modifique o Exercício 2 para incluir uma verificação usando instanceof para garantir
+que o objeto seja do tipo correto antes de fazer o casting. */
 package Desafio02;
 
 public class Principal {
     public static void main(String[] args) {
         Cachorro meuCachorro = new Cachorro();
-        Animal novoAnimal = (Animal) meuCachorro;
+
+        if (meuCachorro instanceof Cachorro) {
+            System.out.println("É um cachorro");
+            Animal novoAnimal = (Animal) meuCachorro;
+        } else {
+            System.out.println("Não é um cachorro");
+        }
     }
 }
