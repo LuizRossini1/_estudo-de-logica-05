1c5c23ae1a3957f2483913b29ac923927d383220
LuizRossini <luiz.guirossini123@gmail.com>
2024-07-17 21:37:04 -0300

    creating class body

--- src/Desafio04/Produto.java
+++ src/Desafio04/Produto.java
@@ -3,5 +3,29 @@ utilize um loop para calcular e imprimir o preço médio dos produtos. */
 package Desafio04;
 
 public class Produto {
+    //Variables
+    private String nome;
+    private double preco;
 
+    //Constructor
+    public Produto(String nome, double preco) {
+        this.nome = nome;
+        this.preco = preco;
+    }
+
+    //Getters
+    public String getNome() {
+        return nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public static void main(String[] args) {
+        //Objects
+        var produto01 = new Produto("Mouse", 99.99);
+        var produto02 = new Produto("Teclado", 110.50);
+        var produto03 = new Produto("Headset", 150.15);
+    }
 }
