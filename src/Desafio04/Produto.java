693db73571a82d8d0c6ed6012d71b8675e4e65d9
LuizRossini <luiz.guirossini123@gmail.com>
2024-07-17 21:50:22 -0300

    implementing logic

--- src/Desafio04/Produto.java
+++ src/Desafio04/Produto.java
@@ -2,18 +2,20 @@
 utilize um loop para calcular e imprimir o preço médio dos produtos. */
 package Desafio04;
 
+import java.util.ArrayList;
+
 public class Produto {
-    //Variables
+    //variables
     private String nome;
     private double preco;
 
-    //Constructor
+    //constructor
     public Produto(String nome, double preco) {
         this.nome = nome;
         this.preco = preco;
     }
 
-    //Getters
+    //getters
     public String getNome() {
         return nome;
     }
@@ -23,9 +25,25 @@ public class Produto {
     }
 
     public static void main(String[] args) {
-        //Objects
-        var produto01 = new Produto("Mouse", 99.99);
-        var produto02 = new Produto("Teclado", 110.50);
-        var produto03 = new Produto("Headset", 150.15);
+        //objects
+        var produto01 = new Produto("Mouse", 159.99);
+        var produto02 = new Produto("Teclado", 130.50);
+        var produto03 = new Produto("Headset", 199.99);
+
+        ArrayList<Produto> lista = new ArrayList<>();
+        lista.add(produto01);
+        lista.add(produto02);
+        lista.add(produto03);
+
+        //loop to add prices
+        double somaPreco = 0;
+        for (Produto produto : lista) {
+            somaPreco += produto.getPreco();
+        }
+
+        //calculate average prices
+        double precoMedio = somaPreco / lista.size();
+
+        System.out.println(precoMedio);
     }
 }
